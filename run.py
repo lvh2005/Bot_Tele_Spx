1673835b54297dfae325a6d9570d65eb0d0f687e
lvh2005 <117277904+lvh2005@users.noreply.github.com>
2026-06-08 01:49:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-```python
 import os
 import subprocess
 import threading
@@ -39,4 +38,3 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"❌ Co loi xay ra khi chay file {bot_file}: {e}")
 
-```
